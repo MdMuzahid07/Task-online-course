0fbb89801bd3be55d46f28e28e6f349551c9face
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-01-29 19:40:40 +0600

    courses page banner

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,8 +1,18 @@
 const Courses = () => {
   return (
-    <div>
-      <h1 className="text-5xl text-red-500">All Courses</h1>
-    </div>
+    <main>
+      <div className="courses-bg">
+        <div className="bg-black bg-opacity-50">
+          <div className="max-w-7xl mx-auto">
+            <div className="min-h-[50vh] w-full flex justify-center items-center">
+              <h1 className="text-4xl sm:text-6xl md:text-[90px] lg:text-[120px] font-semibold text-white">
+                All Courses
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
