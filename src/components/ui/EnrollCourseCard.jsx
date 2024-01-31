e8f40889871429bb6667ddc290ac68709d76ecdc
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-01-31 13:21:18 +0600

    enroll course card Ui/UX

---
+++ src/components/ui/EnrollCourseCard.jsx
@@ -0,0 +1,37 @@
+const EnrollCourseCard = () => {
+  return (
+    <div className=" md:max-w-[800PX] w-full max-h-[800px] md:max-h-[245px] rounded-3xl bg-white hover:drop-shadow p-4 object-center relative  transition delay-100 duration-100 ease-linear mb-5 md:mb-0">
+      <div className="flex flex-col md:flex-row md:gap-4">
+        <div className="w-full md:w-64 h-56 md:h-48">
+          <img
+            src="https://demo.edublink.co/wp-content/uploads/2023/05/girl-1.webp"
+            alt=""
+            className="w-full h-full object-cover object-center bg-red-500 rounded-2xl"
+          />
+        </div>
+        <div>
+          <h1 className="text-2xl">Course Name</h1>
+          <p className="mt-3">
+            <span className="font-semibold">Instructor: </span>John Doe
+          </p>
+
+          <p>
+            <span className="font-semibold">Due Date:</span> 20/2/2024
+          </p>
+
+          <div className="h-3 w-44 md:w-56 lg:w-80 bg-slate-200 rounded-full mt-3 relative">
+            <span
+              className="bg-black h-2 rounded-xl absolute top-0.5 left-0.5"
+              style={{ width: "75%" }}
+            ></span>
+          </div>
+          <button className="mt-4 bg-black text-red-500 font-bold rounded-full px-3 py-1">
+            Continue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EnrollCourseCard;
--- src/pages/dashboard/EnrolledCourses.jsx
+++ src/pages/dashboard/EnrolledCourses.jsx
@@ -1,5 +1,16 @@
+import EnrollCourseCard from "../../components/ui/EnrollCourseCard";
+
 const EnrolledCourses = () => {
-  return <div>EnrolledCourses</div>;
+  return (
+    <main>
+      <div className="text-3xl pt-10">My Courses</div>
+      <section className="mt-14 grid  xl:grid-cols-2 gap-6 lg:gap-10">
+        <EnrollCourseCard />
+        <EnrollCourseCard />
+        <EnrollCourseCard />
+      </section>
+    </main>
+  );
 };
 
 export default EnrolledCourses;
